e5d013bb5f29a9a69b738a2ed1f23ed9916a94d6
manoj <chapagainmanoj35@gmail.com>
2017-11-13 17:17:05 +0545

    analysi

---
+++ browser_analyze/README.md
@@ -0,0 +1,6 @@
+In Ubuntu:
+cd ~/.config/google-chrome/Default
+
+sqlite3 History "select datetime(last_visit_time/1000000-11644473600,'unixepoch'),url from  urls order by last_visit_time desc" > ~/hist.txt
+
+python3 analyze.py
--- chrome.py
+++ chrome.py
@@ -1,15 +1,20 @@
 import os
 import time
 from selenium import webdriver
+
+url = 'https://quay.io/customtrigger/setup/chapagainmanoj/tester'
+
 startTime = time.time()
-chromedriver = "/usr/bin/chromedriver"
+chromedriver = "/usr/local/bin/chromedriver"
 os.environ["webdriver.chrome.driver"] = chromedriver
 driver = webdriver.Chrome(chromedriver)
-driver.get("http://stackoverflow.com")
+driver.get(url)
+
+
 print("Time waiting")
+
 for x in range(0,9):
     print(str(1+x)+' sec',)
     time.sleep(1)
-driver.quit()
-endTime=time.time()
-print("took %s seconds total" % (endTime-startTime))
+
+print(driver)
